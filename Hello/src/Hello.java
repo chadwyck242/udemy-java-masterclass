174d9200c421cdea4b486bbbe745e1a6cc344c05
chadwyck242 <chadwyck.codeart@gmail.com>
2020-01-22 16:15:12 -0700

    Recommit due to errors

---
+++ Hello/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
+    }
+}
---
+++ Primitives/ByteShortIntLong/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println(myMinIntValue);
+        System.out.println(myMaxIntValue);
+    }
+}

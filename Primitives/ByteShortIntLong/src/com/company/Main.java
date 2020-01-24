537b2d13838ba29e1df8c9601482c12877d48343
chadwyck242 <chadwyck.codeart@gmail.com>
2020-01-23 18:35:45 -0700

    Complete Primitive Types Challenge

--- Primitives/ByteShortIntLong/src/com/company/Main.java
+++ Primitives/ByteShortIntLong/src/com/company/Main.java
@@ -10,5 +10,12 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println(myMinIntValue);
         System.out.println(myMaxIntValue);
+
+        byte myByte = 109;
+        short myShort = 30100;
+
+        int myInt = 1245843;
+        long myLong = (long) (50000 + (10 * (myByte + myShort + myInt)));
+        System.out.println(myLong);
     }
 }

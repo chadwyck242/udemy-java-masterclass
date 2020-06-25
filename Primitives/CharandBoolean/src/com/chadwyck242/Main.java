814679233efdd5311f3103abd8b81859e85bf1a7
chadwyck242 <chadwyck.codeart@gmail.com>
2020-06-24 20:27:01 -0600

    Add lesson code for Char and Boolean data types lecture

---
+++ Primitives/CharandBoolean/src/com/chadwyck242/Main.java
@@ -0,0 +1,18 @@
+package com.chadwyck242;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        boolean isCustomerOverTwentyOne = true;
+
+        System.out.println(isCustomerOverTwentyOne);
+
+    }
+}

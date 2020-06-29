3f321bba0508406a9f743c90ada82d94ef9e358b
chadwyck242 <chadwyck.codeart@gmail.com>
2020-06-28 19:47:55 -0600

    Add completed exercise Section 3 Challenge: Operators and Operator Precedence

---
+++ Primitives/OperatorChallenge/src/com/chadwyck242/Main.java
@@ -0,0 +1,26 @@
+package com.chadwyck242;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Code for the Operator and Operator Precedence Challenge
+        // Video 39 of Section 3: First Steps
+
+        double firstValue = 20.00;
+        double secondValue = 80.00;
+
+        double thirdValue = (firstValue + secondValue) * 100.00;
+
+        double fourthValue = thirdValue % 40.00d;
+
+        boolean fifthValue = (fourthValue == 0) ? true : false;
+
+        System.out.println(fifthValue);
+
+        // This should return nothing, as there is no remainder above!
+        if (!fifthValue){
+            System.out.println("Got some remainder");
+        }
+
+    }
+}
---
+++ Primitives/Strings/src/com/chadwyck242/Main.java
@@ -0,0 +1,17 @@
+package com.chadwyck242;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+
+        System.out.println("My string is equal to: " + myString);
+
+        myString = myString + " \u00A9 2020";
+
+        System.out.println(myString);
+        
+
+    }
+}

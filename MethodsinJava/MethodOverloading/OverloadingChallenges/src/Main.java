700d6e44ef59a9b7adbd3d03c9c679b3bdcd3233
chadwyck242 <chadwyck.codeart@gmail.com>
2023-01-07 16:50:11 -0700

    Create new project folder for the Control Flow section of the course.

---
+++ MethodsinJava/MethodOverloading/OverloadingChallenges/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Area of circle: " + AreaCalculator.area(10));
+        System.out.println("Area of square: " + AreaCalculator.area(6, 4));
+
+        MinutesYearsDays.printYearsAndDays(-1000000);
+
+        EqualityPrinter.printEqual(1,4,4);
+
+    }
+}

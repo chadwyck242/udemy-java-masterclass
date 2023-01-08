43306810ca6ffb3ebc7734e14316c2547ea5af10
chadwyck242 <chadwyck.codeart@gmail.com>
2023-01-07 19:50:10 -0700

    Create new project folder for the Control Flow section. Add completed challenge exercises so far from Control Section

--- ControlFlow/src/Main.java
+++ ControlFlow/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
+
+        // Test printNumberInWord method
+        NumberInWord.printNumberInWord(9);
+        NumberInWord.printNumberInWord(-4);
+
+        // Test getDaysInMonth method
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+
+        // Test sumOdd method
+        System.out.println(SumOddRange.sumOdd(2, 15));
 
 
     }
---
+++ ControlFlow/src/SumOddRange.java
@@ -0,0 +1,46 @@
+public class SumOddRange {
+
+    // Method checks if number is odd and returns true, false otherwise
+    public static boolean isOdd(int number) {
+
+        // Check if number is greater than 0
+        if (number < 0) {
+            return false;
+        }
+
+        // Check number is odd and return true
+        if (number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Sum odd numbers within a given range, and return the sum
+    public static int sumOdd(int start, int end) {
+
+        // Initialize the sum variable
+        int sum = 0;
+
+        // Check if start and end are within required constraints
+        if (end < start) {
+            return -1;
+        }
+        else if (start <= 0 || end <= 0) {
+            return -1;
+        }
+
+        // Sum the odd numbers within the given range
+        for(int i = start; i <= end; i++) {
+
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+
+        // Return the sum of off numbers
+        return sum;
+
+    }
+
+}
